e888f3ad74b3cea3bdfcd0c2851c2788a6a3c540
EC2 Default User <ec2-user@ip-172-31-65-251.ec2.internal>
2021-08-31 01:21:10 +0000

    環境変数をセットする

---
+++ cdk/cdk-lang-typescript-apigateway-lambda-nodejs-sample/cdk/lambda/index.ts
@@ -0,0 +1,11 @@
+import {
+    APIGatewayProxyHandler
+} from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+    console.log(event)
+    return {
+        statusCode: 200,
+        body: JSON.stringify(event)
+    }
+}
